9a6ba6a70845aba8b1c641136e8a29b3db609309
Nguyen Truong Bach <bachnguyentruong2003@gmail.com>
2025-03-17 13:56:38 +0700

    upload

---
+++ database.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'Test1';
+$username = 'root';
+$password = '';
+
+try {
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Kết nối thất bại: " . $e->getMessage());
+}
